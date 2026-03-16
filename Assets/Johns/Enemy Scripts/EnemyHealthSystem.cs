e2adba45169a457544d754e7f4d3d2ce9d2ac4b6
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2026-03-16 12:07:38 -0700

    Added Enemy health and my own scripts and my folders

---
+++ Assets/Johns/Enemy Scripts/EnemyHealthSystem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;  
+
+public class EnemyHealthSystem : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
