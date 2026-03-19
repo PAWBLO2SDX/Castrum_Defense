7763159731ef3704df4eada396eca528bc307544
MUKILTEO\1725632 <1725632@mukilteo.wednet.edu>
2026-03-19 13:31:41 -0700

    Add art for the money and the start menu is almost done :)

---
+++ Assets/Eleazar/StartMeunMethods.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class StartMeunMethods : MonoBehaviour
+{
+    public void StartButton()
+    {
+        
+    }
+
+
+    public void QuitButton()
+    {
+        Debug.Log("Quit the game!");
+        Application.Quit();
+    }
+
+
+}
