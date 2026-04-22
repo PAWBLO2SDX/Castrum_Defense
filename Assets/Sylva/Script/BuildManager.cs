223d6974a99bd80580362b247e8bb0dd567a3083
MUKILTEO\1725632 <1725632@mukilteo.wednet.edu>
2026-04-22 13:35:55 -0700

    no message

--- Assets/Sylva/Script/BuildManager.cs
+++ Assets/Sylva/Script/BuildManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 
 public class BuildManager : MonoBehaviour
 {
-    public static BuildManager main;
-
+    private static BuildManager main;
     [Header("References")]
     [SerializeField] private GameObject[] towerPrefabs;
     [SerializeField] private GameObject[] levelManager;
@@ -13,9 +12,11 @@ public class BuildManager : MonoBehaviour
     [SerializeField] private int placeableTowers = 0;
     private int selectedTower;
 
+    public static BuildManager Main { get => main; set => main = value; }
+
     private void Awake()
     {
-        main = this;
+        Main = this;
     }
 
     public int GetPlaceableTowers()
@@ -25,14 +26,14 @@ public class BuildManager : MonoBehaviour
 
     public void LowerTower()
     {
-        main.placeableTowers--;
-        main.towerList.Remove(main.towerList[^1]);
+        Main.placeableTowers--;
+        Main.towerList.Remove(Main.towerList[^1]);
     }
 
     public void IncreaseTower()
     {
-        main.placeableTowers++;
-        main.towerList.Add(main.towerPrefabs[main.selectedTower]);
+        Main.placeableTowers++;
+        Main.towerList.Add(Main.towerPrefabs[Main.selectedTower]);
     }
 
     public List<GameObject> GetTowerList()
